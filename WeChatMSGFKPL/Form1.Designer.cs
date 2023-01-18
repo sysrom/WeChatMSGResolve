19ded5bdd89b02bdaec49a4c5dc98d90e99d0d39
sysrom <88297660+sysrom@users.noreply.github.com>
2023-01-18 16:24:13 +0800

    上传

---
+++ WeChatMSGFKPL/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace WeChatMSGFKPL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 467);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "?:UI很丑 我是傻逼不会画UI|By sysR@M|Github:https://github.com/sysrom|蓝鸽@systemerrorrom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select File/选择文件:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(741, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(579, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "解析表:";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(2, 76);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(798, 388);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(3, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 24);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "打开数据库";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(723, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "转到此表";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(3, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 24);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "关闭数据库";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(564, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "页";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(582, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(24, 21);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(535, 53);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(28, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "+1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(690, 53);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(28, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "-1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(626, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(613, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "WeChatMSGResolve";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WeChatMSGFKPL/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace WeChatMSGFKPL
+{
+    public partial class Form1 : Form
+    {
+        String DBpath;
+        SQLiteConnection DBConnection;
+        string[] strArray;
+        //String[] Coum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog nt = new OpenFileDialog())
+            {
+                nt.Title = "选择要转换的文件";
+                nt.Multiselect = false;
+                if (nt.ShowDialog() == DialogResult.OK)
+                {
+                    DBpath = nt.FileName;
+                    textBox1.Text = nt.FileName;
+                    button2.Enabled = true;
+                    this.Text = this.Text+" Select File:"+nt.FileName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DBConnection = new SQLiteConnection("data source = " +textBox1.Text);
+            DBConnection.Open();
+            SQLiteCommand Tablelist = new SQLiteCommand("select name from sqlite_master where type='table' order by name;", DBConnection);
+            SQLiteDataReader TableReader = Tablelist.ExecuteReader();
+            while (TableReader.Read()) {
+                comboBox1.Items.Add(TableReader["name"]);
+            }
+            comboBox1.SelectedIndex = 1;
+            button2.Enabled = false;
+            button4.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            DBConnection.Close();
+            comboBox1.Items.Clear();
+            button2.Enabled = true;
+            button4.Enabled = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.listView1.Clear();
+            int l = 0;
+            //this.listView1.BeginUpdate();
+            SQLiteCommand GetTableC = new SQLiteCommand($"SELECT COUNT(*) FROM {comboBox1.SelectedItem.ToString()}", DBConnection);
+            SQLiteDataReader CRead = GetTableC.ExecuteReader();
+            while (CRead.Read()) {
+                l=Convert.ToInt32(CRead["COUNT(*)"]);
+            }
+            int ModNum = l % 40;
+            int AllPage = l / 40;
+            if (ModNum != 0 && ModNum > 0) {
+                AllPage++;
+            }
+            label5.Text = $"{AllPage}";
+            List<string> strList = new List<string>();
+            //MessageBox.Show("您选中了表:"+comboBox1.SelectedItem.ToString());
+            SQLiteCommand TableCoum = new SQLiteCommand($"PRAGMA table_info({comboBox1.SelectedItem.ToString()})", DBConnection);
+            SQLiteDataReader TableCoumReader = TableCoum.ExecuteReader();
+            //int a = 0;
+            while (TableCoumReader.Read()) {
+                //this.listView1.Columns.Add(TableCoumReader["name"]);
+                strList.Add(TableCoumReader["name"].ToString());
+                //this.listView1.Columns.Add("ID");
+                this.listView1.Columns.Add($"{TableCoumReader["name"]}", 120, HorizontalAlignment.Left);
+            }
+             strArray = strList.ToArray();
+            Console.WriteLine(strArray.Length);
+            
+            SQLiteCommand TableCoumData = new SQLiteCommand($"select * FROM {comboBox1.SelectedItem.ToString()} LIMIT 0,40", DBConnection);
+            SQLiteDataReader TableCoumDataReader = TableCoumData.ExecuteReader();
+            
+            String NowArry="";
+            int jb = 1;
+            while (TableCoumDataReader.Read()) {
+                ListViewItem add = new ListViewItem();
+                for (int i = 0; i < strArray.Count(); i++) {
+                    if (i == 0)
+                    {
+                        add.Text=Convert.ToString(TableCoumDataReader[i]);
+                    }
+                    else 
+                    {
+                        add.SubItems.Add(Convert.ToString(TableCoumDataReader[i]));
+                    }
+                    Console.WriteLine(TableCoumDataReader[i]);
+                }
+                listView1.Items.Add(add);
+            }
+            
+            //this.listView1.EndUpdate();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if ( Convert.ToInt32(textBox2.Text)+1>Convert.ToInt32(label5.Text)) {
+                MessageBox.Show("超你妈没那么多页（）");
+                return;
+
+            }
+            listView1.Items.Clear();
+            //textBox2.Text = Convert.ToString(Convert.ToInt32(textBox2.Text) + 1);
+            int Limint;
+            Limint = Convert.ToInt32(textBox2.Text);
+            SQLiteCommand TableCoumData = new SQLiteCommand($"select * FROM {comboBox1.SelectedItem.ToString()} LIMIT {(Limint*40)-40},{Convert.ToInt32(textBox2.Text)*40}", DBConnection);
+            SQLiteDataReader TableCoumDataReader = TableCoumData.ExecuteReader();
+
+            String NowArry = "";
+            int jb = 1;
+            while (TableCoumDataReader.Read())
+            {
+                ListViewItem add = new ListViewItem();
+                for (int i = 0; i < strArray.Count(); i++)
+                {
+                    if (i == 0)
+                    {
+                        add.Text = Convert.ToString(TableCoumDataReader[i]);
+                    }
+                    else
+                    {
+                        add.SubItems.Add(Convert.ToString(TableCoumDataReader[i]));
+                    }
+                    Console.WriteLine(TableCoumDataReader[i]);
+                }
+                listView1.Items.Add(add);
+            }
+            textBox2.Text = Convert.ToString(Convert.ToInt32(textBox2.Text) + 1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBox2.Text) - 1 > Convert.ToInt32(label5.Text))
+            {
+                MessageBox.Show("超你妈底裤la（）");
+                return;
+
+            }
+            listView1.Items.Clear();
+            //textBox2.Text = Convert.ToString(Convert.ToInt32(textBox2.Text) + 1);
+            int Limint;
+            Limint = Convert.ToInt32(textBox2.Text);
+            SQLiteCommand TableCoumData = new SQLiteCommand($"select * FROM {comboBox1.SelectedItem.ToString()} LIMIT {(Limint*40)-40},{Convert.ToInt32(textBox2.Text)*40}", DBConnection);
+            SQLiteDataReader TableCoumDataReader = TableCoumData.ExecuteReader();
+
+            String NowArry = "";
+            int jb = 1;
+            while (TableCoumDataReader.Read())
+            {
+                ListViewItem add = new ListViewItem();
+                for (int i = 0; i < strArray.Count(); i++)
+                {
+                    if (i == 0)
+                    {
+                        add.Text = Convert.ToString(TableCoumDataReader[i]);
+                    }
+                    else
+                    {
+                        add.SubItems.Add(Convert.ToString(TableCoumDataReader[i]));
+                    }
+                    Console.WriteLine(TableCoumDataReader[i]);
+                }
+                listView1.Items.Add(add);
+            }
+            textBox2.Text = Convert.ToString(Convert.ToInt32(textBox2.Text) -1);
+        }
+    }
+}
